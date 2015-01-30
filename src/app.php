e4d26bd9daee60d52fd991f33c01d58de66f100f
Everton Inocencio <hewerthomn@gmail.com>
2015-01-30 13:00:11 -0400

    first commit

---
+++ src/app.php
@@ -0,0 +1,27 @@
+<?php
+
+use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
+
+function is_leap_year($year = null)
+{
+	if(null === $year) $year = date('Y');
+
+	return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
+}
+
+$routes = new Routing\RouteCollection();
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+	'year' => null,
+	'_controller' => function($request) {
+		if (is_leap_year($request->attributes->get('year'))) {
+			return new Response('Yep, this is a leap year.');
+		}
+
+		return new Response('Nope, this is a not leap year.');
+	}
+]));
+$routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
+$routes->add('bye', new Routing\Route('/bye'));
+
+return $routes;
